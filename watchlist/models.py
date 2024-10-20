2b03a44153d3637ad17eda8dde66486dea1dd980
Paul <awolesiboluwatife@gmail.com>
2024-10-20 07:43:16 +0200

    added date created field

--- watchlist/models.py
+++ watchlist/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Wallet(models.Model):
     address = models.CharField(max_length=100, unique=True)
     tag = models.CharField(max_length=100, default="Store")
+    date_added = models.DateTimeField(auto_now_add=True) 
 
     def __str__(self):
         return self.address
