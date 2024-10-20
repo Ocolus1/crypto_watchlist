e4dbbdca931d819fa54ba8c584e9f5551d641625
Paul <awolesiboluwatife@gmail.com>
2024-10-20 07:23:11 +0200

    added tag field to wallet

--- watchlist/models.py
+++ watchlist/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Wallet(models.Model):
     address = models.CharField(max_length=100, unique=True)
+    tag = models.CharField(max_length=100, default="Store")
 
     def __str__(self):
         return self.address
